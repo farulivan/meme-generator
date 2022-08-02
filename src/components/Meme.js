55524597f39ee0e0ba3fc71d0ab4517a7cd71a70
Farul Ivan <farulivan@gmail.com>
2022-08-02 13:24:29 +0700

    making proper states

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,20 +2,44 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    // making state
-    const [memePic, randomMeme] = React.useState('');
+    //randomize image state
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     
-    // will start when click on button
-    function getMeme() {
-        // capture memes data as an array
-        const memesArray = memesData.data.memes;
+    //set final image state
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'https://i.imgflip.com/30b1gx.jpg'
+    });
+
+    //function for getting mouse clicked
+    function getMemeImages() {
+        //capture memes data as an array
+        const memesArray = allMemeImages.data.memes;
         // make a random number according tu memes length
         const randomNumber = Math.floor(Math.random() * memesArray.length);
         // put a random number to index of memesArray
         const imageUrl = memesArray[randomNumber].url;
-        // call a state function
-        randomMeme(imageUrl);
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: imageUrl
+        }))
     }
+    
+    // making state (OLD syntax)
+    // const [memePic, randomMeme] = React.useState('');
+
+    // will start when click on button (OLD syntax)
+    // function getMeme() {
+    //     // capture memes data as an array
+    //     const memesArray = memesData.data.memes;
+    //     // make a random number according tu memes length
+    //     const randomNumber = Math.floor(Math.random() * memesArray.length);
+    //     // put a random number to index of memesArray
+    //     const imageUrl = memesArray[randomNumber].url;
+    //     // call a state function
+    //     randomMeme(imageUrl);
+    // }
 
     return (
         <main className="m-9">
@@ -33,12 +57,12 @@ export default function Meme() {
                     name="second-element" className="w-full block px-3 py-2 font-semibold placeholder-gray-400 rounded-md border"
                     />
                 </div>
-                <button className="w-full mt-4 h-10 bg-gradient-to-r from-purple-800 to-purple-600 text-white font-karla font-bold rounded-md" onClick={getMeme}>
+                <button className="w-full mt-4 h-10 bg-gradient-to-r from-purple-800 to-purple-600 text-white font-karla font-bold rounded-md" onClick={getMemeImages}>
                     Get a new meme image <span className="font-sans">🖼</span>
                 </button>
             </div>
             <img className="mt-9 w-full object-contain text-center px-8"
-            src={memePic}>
+            src={meme.randomImage}>
             </img>
         </main>
     )
